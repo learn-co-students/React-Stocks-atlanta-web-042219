577a5db63a7f03e2871e3023004f3fdd9c0f01cd
Han Xu <hanxu27@gmail.com>
2019-07-02 20:59:29 -0400

    Done.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
   return (
     <div>
 
       <strong>Sort by:</strong>
       <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
+        <input type="radio" value="Alphabetically" checked={props.sort === "Alphabetically"} onChange={props.handleSort} />
         Alphabetically
       </label>
       <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
+        <input type="radio" value="Price" checked={props.sort === "Price"} onChange={props.handleSort} />
         Price
       </label>
-      <br/>
+      <br />
 
       <label>
         <strong>Filter:</strong>
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -4,24 +4,63 @@ import PortfolioContainer from './PortfolioContainer'
 import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
+  state = {
+    stocks: [],
+    myStocks: [],
+    showStocks: [],
+    sort: null
+  }
+
+  componentDidMount() {
+    fetch(`http://localhost:3000/stocks`)
+      .then(res => res.json())
+      .then(stocks => this.setState({ stocks, showStocks: stocks }))
+  }
+
+  handleBuy = (stock) => {
+    this.setState({ myStocks: [...this.state.myStocks, stock] })
+  }
+
+  handleSell = (sellStock) => {
+    let newPort = this.state.myStocks
+    const idx = this.state.myStocks.findIndex(stock => stock.id === sellStock.id)
+    newPort.splice(idx, 1)
+    this.setState({ myStocks: newPort })
+  }
+
+  handleSort = e => {
+    this.setState({ sort: e.target.value })
+    let showStocks = this.state.stocks
+    if (e.target.value === 'Alphabetically') {
+      showStocks.sort((a, b) => {
+        if (a.name < b.name) { return -1 }
+        if (a.name > b.name) { return 1 }
+        return 0
+      })
+    } else if (e.target.value === 'Price') {
+      showStocks.sort((a, b) => a.price - b.price)
+    }
+    this.setState({ showStocks })
+  }
+
 
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar handleSort={this.handleSort} sort={this.state.sort} />
 
-          <div className="row">
-            <div className="col-8">
+        <div className="row">
+          <div className="col-8">
 
-              <StockContainer/>
+            <StockContainer stocks={this.state.showStocks} handleBuy={this.handleBuy} />
 
-            </div>
-            <div className="col-4">
-
-              <PortfolioContainer/>
-
-            </div>
           </div>
+          <div className="col-4">
+
+            <PortfolioContainer stocks={this.state.myStocks} handleSell={this.handleSell} />
+
+          </div>
+        </div>
       </div>
     );
   }
